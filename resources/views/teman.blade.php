689b3654a35f70d53ea01ff9e92181d6fcab1b42
DetonaMekanik <tonade236@gmail.com>
2025-10-28 12:11:22 +0800

    updatepertemuan5

--- resources/views/teman.blade.php
+++ resources/views/teman.blade.php
@@ -1,33 +1,30 @@
-@extends('layouts.desain')
-@section('JudulPage','Welcome Page')
-@section('konten')
+@extends('layouts.app')
+@section('JudulPage','Data Teman') 
 
+@section('konten')
 <div class="container">
     <h3>Data Teman</h3>
-    
+
     @if(empty($dt))
         <p>Tidak ada Data</p>
     @else
     <table class="table table-hover">
-
+    <thead>
     <tr>
         <th>ID Buku</th>
         <th>Nama Teman</th>
-        <th>Alamat</th>
-        <th>Kota</th>
-        <th>Telp</th>
-        <th>WA</th>
+        <th></th>
     </tr>
+    </thead>
+    <tbody>
     @foreach($dt as $d)
     <tr>
         <td>{{ $d['idbuku'] }}</td>
         <td>{{ $d['namateman'] }}</td>
-        <td>Jl. Merdeka No. 10</td>
-        <td>Bandung</td>
-        <td>08123456789</td>
-        <td>08123456789</td>
+        <td><a class="btn btn-warning btn-sm" href="{{ route('dtteman.detail',$d['idbuku']) }}"> detail </a></td>
     </tr>
     @endforeach
+    </tbody>
     </table>
     @endif
 
